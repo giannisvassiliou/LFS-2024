8b0d8cc1a64d685a8a743af8c1badc7bcf9cad4f
giannisvassiliou <giannis_vassiliou@yahoo.gr>
2023-07-29 17:43:19 +0300

    Add files via upload

---
+++ code/removeTRAILCOMMA.py
@@ -0,0 +1,15 @@
+from elasticsearch.serializer import np
+import pandas as pd
+
+file1 = open("goodqueriesYAGO1.txt", 'r',encoding="utf8")
+file2 = open("goodqueriesYAGO1R.txt", 'w',encoding="utf8")
+
+Lines = file1.readlines()
+
+for l in Lines:
+    #print(l)
+    l=l.replace(',\n','\n')
+    #print(l)
+    file2.write(l)
+file2.close()
+
